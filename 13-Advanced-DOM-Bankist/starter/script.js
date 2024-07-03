9da95b7e5c54b8c9def23b3d291462c1931b5b42
Ishwar <ishwarkuri4@gmail.com>
2024-07-03 10:04:43 +0530

    added till asych

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,34 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//Tabbed Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+// tabs.forEach(t =>
+//   t.addEventListener('click', () => {
+//     console.log('TAB');
+//   })
+// );
+
+tabsContainer.addEventListener('click', e => {
+  const clickedTab = e.target.closest('.operations__tab');
+  // console.log(clickedTab);
+
+  if (!clickedTab) return;
+
+  // Deactivate all other tabs and contents
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  //Activate Tab
+  clickedTab.classList.add('operations__tab--active');
+
+  // Activate Content Area
+  // console.log(clickedTab.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clickedTab.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
--- complete-javascript-course-updates-and-fixes/14-OOP/starter/script.js
+++ complete-javascript-course-updates-and-fixes/14-OOP/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const User={
+    private user:'user',
+   private mail: 'mail',
+   private password: 'password'
+
+  
+
+}
